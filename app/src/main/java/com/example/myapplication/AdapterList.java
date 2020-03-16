c194678c80e10f27eb784e25ef6d15d43cd93cdc
Arsenko <minsenka@gmail.com>
2020-03-16 10:29:18 +0300

    fixed?

--- app/src/main/java/com/example/myapplication/AdapterList.java
+++ app/src/main/java/com/example/myapplication/AdapterList.java
@@ -11,35 +11,28 @@ import java.util.List;
 import java.util.Map;
 
 public class AdapterList {
-    final String HEADING="heading";
-    final String BODY="body";
-    public List<Map<String, String>> adapterList;
-    public AdapterList(ArrayList<String> ListText){
-        adapterList=prepareContent(ListText);
+    private static final String HEADING = "heading";
+    private static final String BODY = "body";
+    List<Map<String, String>> adapterList;
+
+    public AdapterList(ArrayList<String> ListText) {
+        adapterList = prepareContent(ListText);
     }
 
     @NonNull
     public SimpleAdapter createAdapter(Context context) {
-        return new SimpleAdapter(context,adapterList,R.layout.list_adapter_item,new String[]{HEADING,BODY},new int[]{R.id.heading,R.id.body});
+        return new SimpleAdapter(context, adapterList, R.layout.list_adapter_item, new String[]{HEADING, BODY}, new int[]{R.id.heading, R.id.body});
     }
 
     @NonNull
     private List<Map<String, String>> prepareContent(ArrayList<String> listText) {
-        List<Map<String, String>> temp= new ArrayList<>();
-        for(int i=0;i<listText.size();i++){
-            Map<String,String> mapItem=new HashMap<>();
-            mapItem.put(HEADING,String.valueOf(listText.get(i).length()));
-            mapItem.put(BODY,listText.get(i));
+        List<Map<String, String>> temp = new ArrayList<>();
+        for (int i = 0; i < listText.size(); i++) {
+            Map<String, String> mapItem = new HashMap<>();
+            mapItem.put(HEADING, String.valueOf(listText.get(i).length()));
+            mapItem.put(BODY, listText.get(i));
             temp.add(mapItem);
         }
         return temp;
-    }
-
-    public List<Map<String, String>> getAdapterList() {
-        return adapterList;
-    }
-
-    public void setAdapterList(List<Map<String, String>> adapterList) {
-        this.adapterList = adapterList;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,35 +1,26 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.CharBuffer;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    AdapterList adapterList;
-    SimpleAdapter adapter;
-    ArrayList<String> temp;
+    private AdapterList adapterList;
+    private SimpleAdapter adapter;
+    private ArrayList<String> temp;
     File source;
 
     @Override
@@ -39,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-    public void init(){
-        source=new File(this.getExternalFilesDir(null),"source.txt");
-        if(!source.exists()) {
+    private void init() {
+        source = new File(this.getExternalFilesDir(null), "source.txt");
+        if (!source.exists()) {
             writeToFile(source);
         }
         readFromFile(source);
@@ -49,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 adapterList.adapterList.remove(parent.getItemAtPosition(position));
-                removeFromFile(source,position);
+                removeFromFile(source, position);
                 adapter.notifyDataSetChanged();
             }
         });
 
 
-        final SwipeRefreshLayout refresh=findViewById(R.id.refresh);
+        final SwipeRefreshLayout refresh = findViewById(R.id.refresh);
         refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -66,72 +57,62 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public ListView setContent(){
-        adapterList=new AdapterList(temp);
-        adapter=adapterList.createAdapter(this);
-        ListView listView=findViewById(R.id.myListView);
+    public ListView setContent() {
+        adapterList = new AdapterList(temp);
+        adapter = adapterList.createAdapter(this);
+        ListView listView = findViewById(R.id.myListView);
         listView.setAdapter(adapter);
         return listView;
     }
 
-    private void readFromFile(File source){
-        try {
-            ArrayList<String> lines=new ArrayList<>();
+    private void readFromFile(File source) {
+        try(FileReader fromSource = new FileReader(source)) {
+            ArrayList<String> lines = new ArrayList<>();
             String oneLine;
-            FileReader fromSource = new FileReader(source);
-            BufferedReader br=new BufferedReader(fromSource);
-            while((oneLine=br.readLine())!=null){
+            BufferedReader br = new BufferedReader(fromSource);
+            while ((oneLine = br.readLine()) != null) {
                 lines.add(oneLine);
             }
-            temp=lines;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            temp = lines;
         } catch (IOException e) {
-            e.printStackTrace();
+            Toast.makeText(this,"error while opening file",Toast.LENGTH_LONG).show();
         }
     }
 
-    private void writeToFile(File source){
-        try {
-            FileWriter toSource = new FileWriter(source,false);
+    private void writeToFile(File source) {
+        try (FileWriter toSource = new FileWriter(source, false)) {
             toSource.append(getString(R.string.large_text));
-            toSource.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            Toast.makeText(this,"error while opening file",Toast.LENGTH_LONG).show();
         }
     }
 
-    private void writeStringToFile(File source){
-        try {
-            FileWriter toSource = new FileWriter(source,false);
+    private void writeStringToFile(File source) {
+        try (FileWriter toSource = new FileWriter(source, false)){
             toSource.append(temp.toString());
-            toSource.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            Toast.makeText(this,"error while opening file",Toast.LENGTH_LONG).show();;
         }
     }
 
-    private void removeFromFile(File source, int position){
-        try {
-            ArrayList<String> lines=new ArrayList<>();
+    private void removeFromFile(File source, int position) {
+        try ( FileReader fromSource = new FileReader(source)){
+            ArrayList<String> lines = new ArrayList<>();
             String oneLine;
-            FileReader fromSource = new FileReader(source);
-            BufferedReader br=new BufferedReader(fromSource);
-            int i=0;
-            while((oneLine=br.readLine())!=null){
-                if(i==position){
+            BufferedReader br = new BufferedReader(fromSource);
+            int i = 0;
+            while ((oneLine = br.readLine()) != null) {
+                if (i == position) {
                     i++;
                     continue;
                 }
                 lines.add(oneLine);
                 i++;
             }
-            temp=lines;
+            temp = lines;
             writeStringToFile(source);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            Toast.makeText(this,"error while opening file",Toast.LENGTH_LONG).show();
         }
     }
 }
